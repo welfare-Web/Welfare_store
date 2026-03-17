b7b93cb688397ed28be624f2e5762459c25fadb8
welfare-Web <tlepubwelfare@gmail.com>
2026-03-17 12:29:29 +0530

    Update products.js

--- app/data/products.js
+++ app/data/products.js
@@ -10,55 +10,55 @@ export const categoryDatabase = {
       colors: ['Yellow', 'Metal', 'Blue', 'Silver', 'Lime']
     },
     bestsellers: [
-      { id: 3, title: 'Samsung Galaxy S26 Ultra 5G Smartphone', price: 67.00, oldPrice: 98.00, img: '/mobile/item3/item3_2.png' },
-      { id: 4, title: 'OnePlus 15R 5G Smartphone', price: 117.00, oldPrice: null, img: '/mobile/item4/item4_2.png' },
-      { id: 5, title: 'Motorola Edge 70 Fusion 5G Smartphone', price: 74.00, oldPrice: 92.00, img: '/mobile/item5/item5_2.png' },
+      { id: 3, title: 'Samsung Galaxy S26 Ultra 5G Smartphone', price: 67.00, oldPrice: 98.00, img: '/mobile/item3/Item3_2.png' },
+      { id: 4, title: 'OnePlus 15R 5G Smartphone', price: 117.00, oldPrice: null, img: '/mobile/item4/Item4_2.png' },
+      { id: 5, title: 'Motorola Edge 70 Fusion 5G Smartphone', price: 74.00, oldPrice: 92.00, img: '/mobile/item5/Item5_2.png' },
     ],
     promoImage: [{id:3,img:'/mobile/item3/item3_4.png'}],
     products: [
       {
-        id: 1, title: 'Google Pixel 8 Pro 5G Smartphone', brand: 'Google', color: 'Metal', image: '/mobile/item1/item1_1.png', thumbnails: ['/mobile/item1/item1_2.png', '/mobile/item1/item1_3.png', '/mobile/item1/item1_4.png'], price: 99.22, oldPrice: 121.00, isSale: true, countdown: '293d : 09h : 42m : 48s', stock: 15, sold: 120, rating: 4,
+        id: 1, title: 'Google Pixel 8 Pro 5G Smartphone', brand: 'Google', color: 'Metal', image: '/mobile/item1/Item1_1.png', thumbnails: ['/mobile/item1/Item1_2.png', '/mobile/item1/Item1_3.png', '/mobile/item1/Item1_4.png'], price: 99.22, oldPrice: 121.00, isSale: true, countdown: '293d : 09h : 42m : 48s', stock: 15, sold: 120, rating: 4,
         shortDesc: "Google Pixel 8 Pro is a premium 5G smartphone with Tensor G3 processor, 12GB RAM, triple camera system, and 6.7-inch OLED display, perfect for gaming, photography, and high-performance use.",
         longDesc: "The Google Pixel 8 Pro 5G smartphone is a flagship device designed for powerful performance and professional photography. \nIt comes with the Google Tensor G3 processor, 12GB RAM, and a high-resolution OLED display for smooth and fast performance. \nThe phone features an advanced triple-camera system with AI photography, long-lasting battery, and 5G connectivity, making it perfect for gaming, video recording, and multitasking.With premium build quality and the latest Android features, Pixel 8 Pro offers a smooth and secure smartphone experience.",
         features: ["100% Brand New", "Contains 1 PCS", "Simple and easy"]
       },
       {
-        id: 2, title: 'Apple iPhone 17 Pro 5G Smartphone', brand: 'Apple', color: 'orange', image: '/mobile/item2/item2_1.png', thumbnails: ['/mobile/item2/item2_2.png', '/mobile/item2/item2_3.png', '/mobile/item2/item2_4.png'], price: 500.00, oldPrice: null, isSale: false, stock: 677, sold: 1, rating: 0,
+        id: 2, title: 'Apple iPhone 17 Pro 5G Smartphone', brand: 'Apple', color: 'orange', image: '/mobile/item2/Item2_1.png', thumbnails: ['/mobile/item2/Item2_2.png', '/mobile/item2/Item2_3.png', '/mobile/item2/Item2_4.png'], price: 500.00, oldPrice: null, isSale: false, stock: 677, sold: 1, rating: 0,
         tags: 'bags',
         shortDesc: "Apple iPhone 17 Pro is a high-performance 5G smartphone with powerful processor, advanced triple camera system, OLED display, and premium design for gaming, photography, and professional use.",
         longDesc: "The Apple iPhone 17 Pro is a next-generation smartphone designed with powerful performance and premium build quality. It features a high-resolution OLED display, advanced Apple chipset, and improved camera system for professional photography and smooth multitasking. \n\nWith 5G connectivity, long battery life, and the latest iOS features, this device is perfect for gaming, video recording, and daily use. The modern design and strong performance make it one of the best flagship smartphones.",
         features: ["✔ Display: 6.3-inch Super Retina XDR OLED, 120Hz", "✔ Processor: Apple A19 Pro Chip (Expected)", "✔ RAM: 8GB / 12GB", "✔ Storage: 256GB / 512GB / 1TB", "✔ Rear Camera: 48MP + 48MP + 12MP Triple Camera", "✔ Front Camera: 12MP Selfie Camera", "✔ Battery: Fast Charging + MagSafe Wireless Charging", "✔ Network: 5G / Wi-Fi 6E / Bluetooth", "✔ Operating System: iOS 19 (Expected)", "✔ Face ID & Dynamic Island"]
       },
       {
-        id: 3, title: 'Samsung Galaxy S26 Ultra 5G Smartphone', brand: 'Samsung', color: 'Pink', image: '/mobile/item3/item3_1.png', thumbnails: ['/mobile/item3/item3_2.png', '/mobile/item3/item3_3.png', '/mobile/item3/item3_4.png'], price: 45.00, oldPrice: 76.00, isSale: true, countdown: '293d : 09h : 42m : 48s', stock: 8, sold: 42, rating: 3,
+        id: 3, title: 'Samsung Galaxy S26 Ultra 5G Smartphone', brand: 'Samsung', color: 'Pink', image: '/mobile/item3/Item3_1.png', thumbnails: ['/mobile/item3/Item3_2.png', '/mobile/item3/Item3_3.png', '/mobile/item3/Item3_4.png'], price: 45.00, oldPrice: 76.00, isSale: true, countdown: '293d : 09h : 42m : 48s', stock: 8, sold: 42, rating: 3,
         shortDesc: "Samsung Galaxy S26 Ultra is a flagship 5G smartphone with powerful processor, AI features, 200MP camera, AMOLED display, and S-Pen support, perfect for gaming, photography, and professional use.",
         longDesc: "The Samsung Galaxy S26 Ultra is a premium flagship smartphone built with advanced Galaxy AI technology and ultra-fast performance. It features a large Dynamic AMOLED display with high refresh rate, powerful Snapdragon processor, and a professional-grade camera system. \n\nWith S-Pen support, long battery life, and 5G connectivity, this phone is perfect for productivity, gaming, photography, and multitasking. The premium design and strong build quality make it one of the best high-end smartphones.",
         features: ["✔ Display: 6.8-inch Dynamic AMOLED 2X, 144Hz", "✔ Processor: Snapdragon 8 Gen 4 (Expected)", "✔ RAM: 12GB / 16GB", "✔ Storage: 256GB / 512GB / 1TB", "✔ Rear Camera: 200MP + 50MP + 50MP + 12MP Quad Camera", "✔ Front Camera: 12MP Selfie Camera", "✔ Battery: 5500 mAh with Fast Charging", "✔ Network: 5G / Wi-Fi 7 / Bluetooth", "✔ Operating System: Android 15 + One UI", "✔ S-Pen Support"]
       },
 
       {
-        id: 4, title: 'OnePlus 15R 5G Smartphone', brand: 'OnePlus', color: 'black', image: '/mobile/item4/item4_1.png', thumbnails: ['/mobile/item4/item4_2.png', '/mobile/item4/item4_3.png', '/mobile/item4/item4_4.png'], price: 70.00, oldPrice: 82.00, isSale: true, countdown: '293d : 09h : 42m : 48s', stock: 12, sold: 33, rating: 4,
+        id: 4, title: 'OnePlus 15R 5G Smartphone', brand: 'OnePlus', color: 'black', image: '/mobile/item4/Item4_1.png', thumbnails: ['/mobile/item4/Item4_2.png', '/mobile/item4/Item4_3.png', '/mobile/item4/Item4_4.png'], price: 70.00, oldPrice: 82.00, isSale: true, countdown: '293d : 09h : 42m : 48s', stock: 12, sold: 33, rating: 4,
         shortDesc: "OnePlus 15R is a high-performance 5G smartphone with Snapdragon 8 Gen 5 processor, AMOLED display, 12GB RAM, and long-lasting battery, perfect for gaming, multitasking, and photography.",
         longDesc: "The OnePlus 15R 5G smartphone delivers flagship-level performance with the powerful Snapdragon 8 Gen 5 processor and fast LPDDR5X RAM. It features a large AMOLED display with high refresh rate for smooth gaming and video experience.\n\nThe phone comes with a high-resolution camera system, long-lasting battery, and fast charging support, making it ideal for daily use, gaming, and photography. With OxygenOS and premium design, the OnePlus 15R offers speed, stability, and modern features.",
         features: ["✔ Display: 6.83-inch AMOLED, 165Hz refresh rate", "✔ Processor: Qualcomm Snapdragon 8 Gen 5", "✔ RAM: 12GB / 16GB LPDDR5X", "✔ Storage: 256GB / 512GB UFS 4.1", "✔ Rear Camera: 50MP + 8MP Dual Camera", "✔ Front Camera: 32MP Selfie Camera", "✔ Battery: 7400 mAh with 80W Fast Charging", "✔ Network: 5G / Wi-Fi 7 / Bluetooth", "✔ Operating System: Android 16 with OxygenOS ", "✔ In-display Fingerprint Sensor"]
       },
 
       {
-        id: 5, title: 'Motorola Edge 70 Fusion 5G Smartphone', brand: 'Motorola', color: 'Silver', image: '/mobile/item5/item5_1.png', thumbnails: ['/mobile/item5/item5_2.png', '/mobile/item5/item5_3.png', '/mobile/item5/item5_4.png'], price: 69.00, oldPrice: null, isSale: false, stock: 100, sold: 10, rating: 4,
+        id: 5, title: 'Motorola Edge 70 Fusion 5G Smartphone', brand: 'Motorola', color: 'Silver', image: '/mobile/item5/Item5_1.png', thumbnails: ['/mobile/item5/Item5_2.png', '/mobile/item5/Item5_3.png', '/mobile/item5/Item5_4.png'], price: 69.00, oldPrice: null, isSale: false, stock: 100, sold: 10, rating: 4,
         shortDesc: "Motorola Edge 70 Fusion is a powerful 5G smartphone with AMOLED display, fast processor, AI features, and high-resolution camera, perfect for gaming, photography, and daily use.",
         longDesc: "The Motorola Edge 70 Fusion 5G smartphone is designed for smooth performance and smart AI features. It comes with a powerful Snapdragon processor, high refresh-rate AMOLED display, and advanced camera system for clear photos and videos.\n\nWith Moto AI features, long battery life, and fast charging support, this phone is perfect for multitasking, gaming, and professional use. The slim and premium design makes it comfortable to hold and stylish to use.",
         features: ["✔ Display: 6.7-inch Full HD+ AMOLED, 144Hz", "✔ Processor: Qualcomm Snapdragon 8s Gen 3", "✔ RAM: 8GB / 12GB", "✔ Storage: 256GB / 512GB", "✔ Rear Camera: 50MP + 13MP Dual Camera", "✔ Front Camera: 32MP Selfie Camera", "✔ Battery: 5000 mAh with 68W Fast Charging", "✔ Network: 5G / Wi-Fi 6 / Bluetooth", "✔ Operating System: Android 15", "✔ In-display Fingerprint Sensor", "✔ Moto AI Features Support"]
       },
 
       {
-        id: 6, title: 'Apple MacBook Neo 14-inch Laptop', brand: 'Apple', color: 'Silver', image: '/mobile/item6/item6_1.png', thumbnails: ['/mobile/item6/item6_2.png', '/mobile/item6/item6_3.png', '/mobile/item6/item6_4.png'], price: 60.00, oldPrice: 69.00, isSale: false, stock: 2, sold: 500, rating: 5,
+        id: 6, title: 'Apple MacBook Neo 14-inch Laptop', brand: 'Apple', color: 'Silver', image: '/mobile/item6/Item6_1.png', thumbnails: ['/mobile/item6/Item6_2.png', '/mobile/item6/Item6_3.png', '/mobile/item6/Item6_4.png'], price: 60.00, oldPrice: 69.00, isSale: false, stock: 2, sold: 500, rating: 5,
         shortDesc: "Apple MacBook Neo is a powerful and lightweight laptop with Apple M3 chip, Retina display, long battery life, and fast SSD storage, perfect for work, coding, editing, and daily use.",
         longDesc: "The Apple MacBook Neo 14-inch laptop is designed for high performance and smooth multitasking. Powered by the Apple M3 chip, it delivers fast speed, low power consumption, and excellent graphics performance.\n\nIt features a Retina display with sharp colors, long battery life, and fast SSD storage for quick loading. The slim and lightweight design makes it perfect for students, professionals, and creators.",
         features: ["✔ Display: 14-inch Liquid Retina Display", "✔ Processor: Apple M3 Chip", "✔ RAM: 16GB Unified Memory", "✔ Storage: 512GB / 1TB SSD", "✔ Graphics: Apple Integrated GPU", "✔ Battery: Up to 18 Hours Backup", "✔ Ports: USB-C / Thunderbolt / Headphone Jack", "✔ Operating System: macOS", "✔ Wi-Fi 6E / Bluetooth 5.3", "✔ Backlit Magic Keyboard", "✔ Touch ID Security"]
       },
 
       {
-        id: 7, title: 'HP Victus Gaming Laptop i5 13th Gen RTX 3050', brand: 'HP', color: 'Silver', image: '/mobile/item7/item7_1.png', thumbnails: ['/mobile/item7/item7_2.png', '/mobile/item7/item7_3.png', '/mobile/item7/item7_4.png'], price: 98.00, oldPrice: null, isSale: false, stock: 5, sold: 300, rating: 5,
+        id: 7, title: 'HP Victus Gaming Laptop i5 13th Gen RTX 3050', brand: 'HP', color: 'Silver', image: '/mobile/item7/Item7_1.png', thumbnails: ['/mobile/item7/Item7_2.png', '/mobile/item7/Item7_3.png', '/mobile/item7Iitem7_4.png'], price: 98.00, oldPrice: null, isSale: false, stock: 5, sold: 300, rating: 5,
         shortDesc: "HP Victus gaming laptop comes with Intel i5 13th Gen processor, RTX 3050 graphics, 16GB RAM, and 512GB SSD, perfect for gaming, editing, coding, and high-performance work.",
         longDesc: "The HP Victus Gaming Laptop is built for high performance and smooth gaming experience. Powered by Intel Core i5-13420H processor and NVIDIA RTX 3050 graphics, it delivers fast speed and powerful graphics for modern games and heavy applications.\n\n With 16GB RAM, 512GB SSD, and Windows 11, this laptop ensures smooth multitasking and quick loading. The high-quality display and strong cooling system make it ideal for gamers, students, and professionals.",
         features: ["✔ Display: 15.6-inch Full HD IPS Display", "✔ Processor: Intel Core i5-13420H (13th Gen)", "✔ Graphics: 4GB NVIDIA GeForce RTX 3050", "✔ RAM: 16GB DDR4 3200 MHz", "✔ Storage: 512GB PCIe SSD", "✔ Operating System: Windows 11 Home", "✔ MS Office 2024 Lifetime", "✔ Microsoft 365 – 1 Year Free", "✔ Wi-Fi 6 / Bluetooth 5.3", "✔ Backlit Keyboard", "✔ High Performance Cooling System"]
@@ -66,21 +66,21 @@ export const categoryDatabase = {
 
 
       {
-        id: 8, title: 'Dell Inspiron DB14255 Ryzen 7 AI Laptop', brand: 'Dell', color: 'Metal', image: '/mobile/item8/item8_1.png', thumbnails: ['/mobile/item8/item8_2.png', '/mobile/item8/item8_3.png', '/mobile/item8/item8_4.png'], price: 499.00, oldPrice: null, isSale: false, stock: 20, sold: 15, rating: 3,
+        id: 8, title: 'Dell Inspiron DB14255 Ryzen 7 AI Laptop', brand: 'Dell', color: 'Metal', image: '/mobile/item8/Item8_1.png', thumbnails: ['/mobile/item8/Item8_2.png', '/mobile/item8/Item8_3.png', '/mobile/item8/Item8_4.png'], price: 499.00, oldPrice: null, isSale: false, stock: 20, sold: 15, rating: 3,
         shortDesc: "Dell Inspiron DB14255 laptop comes with AMD Ryzen 7 AI processor, 16GB RAM, 1TB SSD, and 14-inch FHD+ display, perfect for work, coding, editing, and multitasking.  ",
         longDesc: "The Dell Inspiron DB14255 is a powerful and lightweight laptop designed for productivity and performance. It is powered by AMD Ryzen 7 350 AI processor with Radeon graphics for fast speed and smooth performance.\n\n With 16GB LPDDR5X RAM and 1TB SSD storage, the laptop handles multitasking, office work, and creative tasks easily.The 14-inch FHD+ IPS display gives clear visuals, and Windows 11 with Office 2024 makes it ready for professional use",
         features: ["✔ Display: 14.0-inch FHD+ IPS, 300 nits", "✔ Processor: AMD Ryzen 7 350 AI", "✔ Graphics: AMD Radeon Graphics", "✔ RAM: 16GB LPDDR5X", "✔ Storage: 1TB SSD", "✔ Operating System: Windows 11", "✔ MS Office Home & Student 2024", "✔ Battery: 4-Cell 64Wh"]
       },
 
       {
-        id: 9, title: 'Lenovo IdeaPad Slim Gaming i7 13th Gen Laptop', brand: 'Lenovo', color: 'Metal', image: '/mobile/item9/item9_1.png', thumbnails: ['/mobile/item9/item9_2.png', '/mobile/item9/item9_3.png', '/mobile/item9/item9_4.png'], price: 499.00, oldPrice: null, isSale: false, stock: 20, sold: 15, rating: 3,
+        id: 9, title: 'Lenovo IdeaPad Slim Gaming i7 13th Gen Laptop', brand: 'Lenovo', color: 'Metal', image: '/mobile/item9/Item9_1.png', thumbnails: ['/mobile/item9/Item9_2.png', '/mobile/item9/Item9_3.png', '/mobile/item9/Item9_4.png'], price: 499.00, oldPrice: null, isSale: false, stock: 20, sold: 15, rating: 3,
         shortDesc: "Lenovo IdeaPad Slim Gaming laptop comes with Intel i7 13th Gen processor, IPS display, metal body design, and fast performance, perfect for gaming, coding, editing, and office work.",
         longDesc: "The Lenovo IdeaPad Slim Gaming laptop is built for powerful performance and smooth multitasking. It features Intel Core i7-13620H processor, high-quality IPS display, and premium metal design for durability.\n\n With fast RAM, SSD storage, and Windows 11 with Office 2024, this laptop is perfect for gaming, professional work, programming, and content creation.\n\n Its thin and lightweight build makes it easy to carry while still delivering high performance.",
         features: ["✔ Display: 15.3-inch WUXGA IPS Display", "✔ Processor: Intel Core i7-13620H (13th Gen)", "✔ Graphics: Integrated Intel Graphics", "✔ RAM: 16GB DDR5", "✔ Storage: 512GB / 1TB SSD", "✔ Operating System: Windows 11", "✔ MS Office Home & Student 2024", "✔ Body: Metal A Cover Design"]
       },
 
       {
-        id: 10, title: 'ASUS ExpertBook Business Laptop 15.6-inch FHD', brand: 'ASUS', color: 'Metal', image: '/mobile/item10/item10_1.png', thumbnails: ['/mobile/item10/item10_2.png', '/mobile/item10/item10_3.png', '/mobile/item10/item10_4.png'], price: 499.00, oldPrice: null, isSale: false, stock: 20, sold: 15, rating: 3,
+        id: 10, title: 'ASUS ExpertBook Business Laptop 15.6-inch FHD', brand: 'ASUS', color: 'Metal', image: '/mobile/item10/Item10_1.png', thumbnails: ['/mobile/item10/Item10_2.png', '/mobile/item10/Item10_3.png', '/mobile/item10/Item10_4.png'], price: 499.00, oldPrice: null, isSale: false, stock: 20, sold: 15, rating: 3,
         shortDesc: "ASUS ExpertBook is a lightweight business laptop with FHD display, fast SSD storage, Wi-Fi 6, multiple ports, and strong performance, perfect for office work, coding, and daily use.",
         longDesc: "The ASUS ExpertBook laptop is designed for business users and professionals who need reliable performance and strong connectivity. It features a Full HD display, fast processor, and SSD storage for smooth multitasking.\n\n With multiple USB ports, HDMI, RJ-45 Ethernet, Wi-Fi 6, and Bluetooth 5.4, this laptop provides excellent connectivity options.\n\n Its slim and lightweight design makes it easy to carry, while Windows 11 ensures a modern and secure experience.",
         features: ["✔ Display: 15.6-inch Full HD Display", "✔ Processor: Intel Core i5 / Ryzen 5 (Business Series)", "✔ RAM: 16GB DDR4", "✔ Storage: 512GB SSD", "✔ Graphics: Integrated Graphics", "✔ Operating System: Windows 11", "✔ Ports: USB-C, USB-A, HDMI, RJ-45, Audio Jack", "✔ Fingerprint / Security Lock Support"]
